3937e41aff90ac1cb2f9819976b171bcf3b7d811
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-01-29 21:51:46 -0300

    nix: fix hyprland envars

--- hosts/caladan/configuration.nix
+++ hosts/caladan/configuration.nix
@@ -91,7 +91,7 @@
     LIBVA_DRIVER_NAME = "nvidia"; 
     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     NVD_BACKEND = "direct";
-    ELECTRON_OZONE_PLATFORM_HINT = "auto";
+    #ELECTRON_OZONE_PLATFORM_HINT = "auto";
   };
 
   services.displayManager = {
