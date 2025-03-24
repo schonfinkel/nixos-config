8cda57b8222051bd42ffc5a58cbfd0792815dd06
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-03-24 19:50:57 -0300

    nix: update flakes + add swap to workstation + add slack

--- hosts/caladan/hardware-configuration.nix
+++ hosts/caladan/hardware-configuration.nix
@@ -28,7 +28,14 @@
       fsType = "ext4";
     };
 
-  swapDevices = [ ];
+  zramSwap.enable = true;
+
+  swapDevices = [
+    {
+      device = "/swapfile";
+      size = 16 * 1024;
+    }
+  ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
