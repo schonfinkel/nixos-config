955f26736f53b8d9f0d9024d14064eff177d82a2
Marcos Benevides <marcos.schonfinkel@gmail.com>
2025-11-27 17:32:31 -0400

    nix: update flakes

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -78,10 +78,11 @@ in
         settings = {
           # Monitors
           monitor = [
-            # "HDMI-A-1,highres,0x0,2"
+            "HDMI-A-1,highres,0x0,2"
             "eDP-1,highres,1920x0,1"
             # "HDMI-A-1,preferred,auto,1,mirror,eDP-1"
-            ",preferred,auto,2,mirror,eDP-1"
+            # ",preferred,auto,2,mirror,eDP-1"
+            # "HDMI-A-1,preferred,auto,2"
           ];
 
           # Programs
